054d20d2027a3986751ed1bb21363e1bf032ca38
Vitor Livi <vitor@pop-os.localdomain>
2023-01-29 09:50:22 -0300

    feat: create customer reward points unit test

--- src/entity/customer.spec.ts
+++ src/entity/customer.spec.ts
@@ -1,4 +1,4 @@
-import {Address} from "./address";
+import { Address } from "./address";
 import { Customer } from "./customer";
 
 describe("Customer unit tests", () => {
@@ -35,7 +35,18 @@ describe("Customer unit tests", () => {
 
   it("should throw erro when address is undefined", () => {
     const customer = new Customer("123", "John Doe");
-  
+
     expect(() => customer.activate()).toThrowError("Address is undefined");
+  });
+
+  it("should add reward points", () => {
+    const customer = new Customer("123", "John Doe");
+    expect(customer.rewardPoints).toBe(0);
+
+    customer.addRewardPoints(10);
+    expect(customer.rewardPoints).toBe(10);
+
+    customer.addRewardPoints(20);
+    expect(customer.rewardPoints).toBe(30);
   });
 });
